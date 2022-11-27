7587a1296ed593a9e7753ede2b83d20288404d29
GDV1 <gabrieldv.dv@hotmail.com>
2022-11-26 21:30:37 -0300

    Setting api

--- src/app/shared/service/api-service.service.ts
+++ src/app/shared/service/api-service.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { Deputados } from '../typings/data';
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +8,7 @@ import { Injectable } from '@angular/core';
 export class ApiServiceService {
 
   constructor() { }
+
+  private readonly urlDeputados = '/deputados';
+  // private deputados$: BehaviorSubject<Array<Deputados>>;
 }
